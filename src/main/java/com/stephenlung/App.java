50968556291e2f143cfd65ca3d4dc4899b1390ce
stephenlung <stephenlung05@gmail.com>
2020-08-01 19:47:15 +0000

    first commit

---
+++ src/main/java/com/stephenlung/App.java
@@ -0,0 +1,13 @@
+package com.stephenlung;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
